090b07f219c9241ed3a94754a3fec0b12bda0828
Vedic Sharma <mevs2000@gmail.com>
2018-03-23 14:36:26 +0530

    forward pass and back prop layout

---
+++ Assignment2/NN/main.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class NN
+{
+    int layers;
+    vector <int> sizes;
+    vector <vector <vector <double> > > weights, delw;
+    vector <vector <double> > z, delta;
+    vector <double> target;
+    NN(int layers, vector <int> &sizes, vector <double> &input, vector <double> &t)
+    {
+        this->layers = layers;
+        z.push_back(input);
+        for (int i = 0; i < layers; ++i)
+        {
+            this->sizes[i] = sizes[i];
+            delta.push_back(vector <double> (sizes[i]));
+        }
+        for (int i = 0; i < layers - 1; ++i)
+        {
+            weights.push_back(vector <vector <double> > (sizes[i], vector <double> (sizes[i + 1])));
+            delw.push_back(vector <vector <double> > (sizes[i], vector <double> (sizes[i + 1])));
+            z.push_back(vector <double> (sizes[i]));
+        }
+        target = t;
+    }
+    void forwardPass()
+    {
+        for (int i = 1; i < layers; ++i)
+        {
+            for (int j = 0; j < sizes[i]; ++j)
+            {
+                z[i][j] = 0;
+                for (int k = 0; k < sizes[i - 1]; ++k)
+                    z[i][j] += weights[i - 1][k][j] * z[i - 1][k];
+                z[i][j] = sigmoid(z[i][j]);
+            }
+        }
+    }
+    void backProp()
+    {
+        forwardPass();
+        for (int i = 0; i < sizes[layers - 1]; ++i)
+            delta[layers - 1][i] = z[layers - 1][i] - target[i];
+        for (int i = layers - 2; i >= 0; --i)
+        {
+            for (int j = 0; j < sizes[i]; ++j)
+            {
+                delta[i][j] = 0;
+                for (int k = 0; k < sizes[i + 1]; ++k)
+                    delta[i][j] += weights[i + 1][j][k] * delta[i + 1][k];
+                delta[i][j] = sigmoid(z[i][j]) * (1 - sigmoid(z[i][j])) * delta[i][j];
+            }
+        }
+        for (int i = 0; i < layers - 1; ++i)
+            for (int j = 0; j < sizes[i]; ++j)
+                for (int k = 0; k < sizes[i + 1]; ++k)
+                    delw[i][j][k] = delta[i + 1][k] * z[i][j];
+    }
+};
+
+int main()
+{
+    Matrix m1(2, 2), m2(2, 2);
+    m1.A[0][0] = 0; m1.A[0][1] = 1;
+    m1.A[1][0] = 2; m1.A[1][1] = 3;
+    m2.A[0][0] = 1; m2.A[0][1] = 2;
+    m2.A[1][0] = 1; m2.A[1][1] = 2;
+    m1.binaryOp(m2, plus<double>());
+    cout << m1.A[0][0] << " " << m1.A[0][1] << endl;
+    cout << m1.A[1][0] << " " << m1.A[1][1] << endl;
+    return 0;
+}
